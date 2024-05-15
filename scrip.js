05b109017c36daf0472c7c52988e8f0b081dbf07
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-15 13:32:58 +0700

    finish

---
+++ scrip.js
@@ -0,0 +1,21 @@
+//ex1
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    console.log(myAlphabet.length);
+
+//ex1.2
+let Planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter'];
+
+Planets.forEach((planet, index) => {
+    console.log(`Planet at index ${index}: ${planet}`);
+});
+
+//ex1.3
+let myArr = [ 1, 2, 'One', true];
+myArr.forEach((myArr, index) => {
+    console.log(`${index}: ${myArr}`);
+});
+
+
+
+
+
